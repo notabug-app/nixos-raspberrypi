c429dbd8a7964e00d801411ce884cd3fd9733c23
s-Sizz <71391035+s-Sizz@users.noreply.github.com>
2026-07-15 12:25:58 +0300

    use nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Wrapper over nvmd/nixos-raspberrypi with 7.x kernels";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixos-raspberrypi = {
       url = "github:nvmd/nixos-raspberrypi";
